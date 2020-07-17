03cd433c7be8cb82c0b896fa3f2bca74ee4a281a
seanaleid <seanaleid@gmail.com>
2020-07-17 12:17:54 -0400

    Complete ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,15 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.index = 0
+        self.capacity = capacity
+        self.data = []
 
     def append(self, item):
-        pass
+        if len(self.data) == self.capacity:
+            self.data[self.index] = item
+        else:
+            self.data.append(item)
+        self.index = (self.index + 1) % self.capacity
 
     def get(self):
-        pass
+        return self.data
